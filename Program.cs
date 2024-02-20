ad4d94dc4647dd1cd47a41e08d3c848698d214aa
Filipi Maciel <filipimacielrj@gmail.com>
2024-02-20 10:23:08 -0300

    Git inicial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Qual seu nome?");
+//var name = Console.ReadLine();
+//var currentDate = DateTime.Now; //classe static com um atributo
+//Console.WriteLine("Ola " + name + ", em " + currentDate + "!");
+
+int a = 5;
+int b = a + 2; 
+
+double x = 3/2.0; // double precisa que pelo menos um valor seja no formato com .0
+
+float y = 3f/2 + 3/2; // o "f" depois do número deve estar presente em pelo menos um dos valores da operação que gere fracionários
+
+Console.WriteLine(y);
+
+/*
+TIPOS INTERNOS 
+
+Vantagem:
+- Tipos únicos podem ser padronizados, todos vão herdar as características
+
+Desvantagem: 
+- Lento devido as conversões que devem ser feitas
+
+*/
