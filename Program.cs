d17f7cbc561479206ab7d2f498c14234654fe69e
Filip3ra <filipimacielrj@gmail.com>
2024-02-20 14:04:33 -0300

    tentando fazer rodar

--- Program.cs
+++ Program.cs
@@ -5,13 +5,15 @@ Console.WriteLine("Qual seu nome?");
 //Console.WriteLine("Ola " + name + ", em " + currentDate + "!");
 
 int a = 5;
-int b = a + 2; 
+int b = a + 2;
 
-double x = 3/2.0; // double precisa que pelo menos um valor seja no formato com .0
+double x = 3 / 2.0; // double precisa que pelo menos um valor seja no formato com .0
 
-float y = 3f/2 + 3/2; // o "f" depois do número deve estar presente em pelo menos um dos valores da operação que gere fracionários
+float y = 3f / 2 + 3 / 2; // o "f" depois do número deve estar presente em pelo menos um dos valores da operação que gere fracionários
 
 Console.WriteLine(y);
+
+
 
 /*
 TIPOS INTERNOS 
@@ -41,13 +43,14 @@ Usar nomes significativos
 */
 
 
-int[] nums = {1,2,3,4,5};
-int [] nums2 = nums;
-nums2[3] = 5;
-Console.WriteLine(nums[3]); 
 
-typeof type = 12345.Getype();
-Console.WriteLine(type); 
+int[] nums = { 1, 2, 3, 4, 5 };
+int[] nums2 = nums;
+nums2[3] = 5;
+Console.WriteLine(nums[3]);
+
+
+Console.WriteLine("Hey there!");
 
 
 
