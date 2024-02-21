665f03a35f7107200c43c9eb05a98b97ed3eadc6
Filip3ra <filipimacielrj@gmail.com>
2024-02-21 14:13:09 -0300

    Atualização com exercício 1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System; // Dependendo da versão, "diretivas" não são necessárias
+﻿/* using System; // Dependendo da versão, "diretivas" não são necessárias
 
 namespace ConsoleApp1
 {
@@ -18,14 +18,15 @@ namespace ConsoleApp1
             string name = Console.ReadLine();
             Console.WriteLine($"Hello {name}");
        
-      Console.WriteLine("Qual o seu nome?");
-      var name = Console.ReadLine();
-      var currentDate = DateTime.Now;
-      
-      // Duas formas de imprimir
-      Console.WriteLine("Olá " + name + ", em " + currentDate);
-      Console.WriteLine($"Hello {name} in {currentDate}.");
-      */
+            Console.WriteLine("Qual o seu nome?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            
+            // Duas formas de imprimir
+            Console.WriteLine("Olá " + name + ", em " + currentDate);
+            Console.WriteLine($"Hello {name} in {currentDate}.");
+            */
+
       /* Valores máximos e mínimos de alguns tipos
             bool canIVote = true;
             Console.WriteLine("Biggest Integer : {0}", int.MaxValue);
@@ -43,19 +44,17 @@ namespace ConsoleApp1
             Console.WriteLine("Smallest Decimal : {0}", Decimal.MinValue);
        
 
+            Console.WriteLine("Biggest Double : {0}", Double.MaxValue);
+            double dbPiVal = 3.14159265358979;
+            double dbBigNum = 3.0000000000002;
+            Console.WriteLine("DBL : PI + BigNum = {0}", dbPiVal + dbBigNum);
 
 
-      Console.WriteLine("Biggest Double : {0}", Double.MaxValue);
-      double dbPiVal = 3.14159265358979;
-      double dbBigNum = 3.0000000000002;
-      Console.WriteLine("DBL : PI + BigNum = {0}", dbPiVal + dbBigNum);
-
-
-      Console.WriteLine("Biggest Float : {0}", float.MaxValue);
-      float fltPiVal = 3.141592F;
-      float fltBigNum = 3.00002F;
-      Console.WriteLine("FLT : PI + BigNum = {0}", fltPiVal + fltBigNum);
-*/
+            Console.WriteLine("Biggest Float : {0}", float.MaxValue);
+            float fltPiVal = 3.141592F;
+            float fltBigNum = 3.00002F;
+            Console.WriteLine("FLT : PI + BigNum = {0}", fltPiVal + fltBigNum);
+            */
       /* Conversões de tipos
             bool boolFromStr = bool.Parse("true");  // parse de um tipo pro outro
             int inFromStr = int.Parse("100");
@@ -74,9 +73,13 @@ namespace ConsoleApp1
             long longNum = intNum;
             Console.WriteLine("LongNum : " + longNum);
 
-       */
+       
 
-
+      
+      
+      
+ 
+      
 
 
 
@@ -84,6 +87,106 @@ namespace ConsoleApp1
     }
   }
 }
+ */
 
 
+
+
+/* 1. É possível comparar tanto tipos de valor 
+      quanto tipos de referência usando o método Equals? 
+      Dê, pelo menos, três exemplos distintos de uso com 
+      tipos de valor e referência. 
+
+      R: Na documentação é especificado que a funcionalidade "Equals" é 
+      usada para comparar dois objetos. Mais comumente para identificar se um
+      objeto é nulo ou não.
+
+      Ao tentar comparar tipos de valores (int, string, etc) com tipos de referência
+      (objetos, vetores, etc) o retorno da função sempre será falso.
+
+
+      using System;
+
+      public class Program
+      {
+      public static void Main()
+      {
+            int n = 3;
+            float f = 1.2F;
+            string s = "Filipi";
+            object o1 = n;
+            object o2 = n;
+            Dog d1 = new Dog("nina");
+            Dog d2 = new Dog("nina");
+            Dog d3 = d2;
+            int[] vet = {1,2,3,4,5};
+
+            Console.WriteLine("objeto e int : {0}", n.Equals(o1));
+            Console.WriteLine("objeto e objeto : {0}", o1.Equals(o2));
+            Console.WriteLine("objeto e float : {0}", o1.Equals(f));
+            Console.WriteLine("Dog e objeto : {0}", d1.Equals(o1));
+            Console.WriteLine("Dog e int : {0}", d1.Equals(n));
+            Console.WriteLine("Dog e float : {0}", d1.Equals(f));
+            Console.WriteLine("Dog("+d1.GetType()+") e string("+s.GetType()+") : {0}", d1.Equals(s));
+            Console.WriteLine("Dog e Dog : {0}", d1.Equals(d2));
+            Console.WriteLine("Dog e Dog : {0}", object.Equals(d1, d2));
+            Console.WriteLine("Dog3 e Dog2 : {0}", object.Equals(d3, d2));
+            Console.WriteLine("array e int : {0}", object.Equals(vet, n));
+            Console.WriteLine("array[0] e int : {0}", object.Equals(vet[0], n));
+            
+
+
+      }
+      }
+
+
+      public class Dog
+      {
+      // Public field.
+      public string name;
+
+      // Class constructor.
+      public Dog(string dogName)
+      {
+            this.name = dogName;
+      }
+      }
+
+*/
+
+/* 2. Como atribuir o valor de um array 'a' para um array 'b' e, em seguida, 
+modificar 'b' sem afetar o array original?
+
+R: A documentação explica que: 
+"Variables of reference types store references to their data (objects), 
+while variables of value types directly contain their data. 
+With reference types, two variables can reference the same object; therefore, 
+operations on one variable can affect the object referenced by the other variable."
+
+Então se usarmos o operador de atribuição "=" não estamos copiando um array pro outro,
+estamos 
+*/
+
+
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+
+      int[] a = {1,2,3,4,5};
+      int[] b = a;
+
+      Console.WriteLine(a[0]);
+      Console.WriteLine(b[0]);
+
+      this.b[0] = -2;
+
+      Console.WriteLine(a[0]);
+      Console.WriteLine(b[0]);
+
+    }
+}
 
--- obj/Debug/net7.0/DotNet.AssemblyInfo.cs
+++ obj/Debug/net7.0/DotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+434ab26c6dc07baba284e5fa4370c563d04e29e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d5554370b3d9441b98dfb75aed01100cb5796ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
